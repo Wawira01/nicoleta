67e57e64bb42f15301c1a0d3a459b310508712bb
Wawira01 <58140545+Wawira01@users.noreply.github.com>
2019-11-24 17:24:54 +0300

    Add files via upload

---
+++ school/bs.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">gift school</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Selections<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="">Page 1-1</a></li>
+          <li><a href="Contact.html">Contact</a></li>
+          <li><a href="home.html">back</a></li>
+        </ul>
+      </li>
+      <li><a href="Contact Us.html">Contact</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+  <h3>Navbar With Dropdown</h3>
+  <p>This example adds a dropdown menu for the "Page 1" button in the navigation bar.</p>
+  
+</div>
+<P>Gift Academy  School is a co-educational international school in Nairobi, Kenya. 
+We teach children from age 3 to 18, following the National Curriculum of England and Wales. 
+We are a member of the Braeburn Group of International Schools.
+The   Gift Academyits beginnings in 200 when a co-educational school called the Nairobi European School began in buildings designed for police Barracks. In 1931 the boys were separated from the girls. In 1935, the school was renamed The European Girls Secondary School and had its first Headmistress, Miss Kerby appointed.</P>
+		
+
+</body>
+</html>
+
